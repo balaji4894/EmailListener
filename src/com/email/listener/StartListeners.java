41154ea5a4b23eb2858494ca31bd4c3473d5cf4e
Ben <ben512@hotmail.co.uk>
2014-01-21 21:09:37 +0000

    Added limit to mount of loop times

--- src/com/email/listener/StartListeners.java
+++ src/com/email/listener/StartListeners.java
@@ -7,22 +7,25 @@ public class StartListeners {
 	
 	public static void main(String[] args)
 	{
+		int i =1;
 		
-		while (true)
+		while (i <100)
 		{
-		
+		System.out.println("************Start up attempt "+i);
 		INSTANCE = new ListenToEmails(); 
 		Thread thread = new Thread(INSTANCE);
 		thread.start(); 
 		System.out.println("Started new thread");
-		while(thread.isAlive())
-		{
-			
-			
-			
-		}
-		System.out.println("thread died");
 		
+			while(thread.isAlive())
+			{
+				
+				
+				
+			}
+		
+			System.out.println("thread died");
+		i++;
 		}
 		
 		
